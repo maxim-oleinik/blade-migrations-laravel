06d03d23b9819ad21a3323868fa67cfcb2b3af72
Maxim Oleinik <maxim.oleinik@gmail.com>
2019-07-06 19:55:43 +0300

    ServiceProvider: remove DbAdapter from provides()
    It can confilct with main project settings.
    Besides it will by registered anyway with call DbRepository (if not defined earlier)

--- src/MigrationsServiceProvider.php
+++ src/MigrationsServiceProvider.php
@@ -43,7 +43,6 @@ class MigrationsServiceProvider extends ServiceProvider
     public function provides()
     {
         return [
-            DbAdapter::class,
             DbRepository::class,
             FileRepository::class,
             MigrationService::class,
